f1abc379c885b4ab1e39868f76fda153895ea2a6
Alexander <alewin1994@gmail.com>
2022-07-15 22:26:53 +0200

    added whip attack and pickup

--- RW gamejam/Scripts/Player.gd
+++ RW gamejam/Scripts/Player.gd
@@ -4,22 +4,18 @@ extends KinematicBody2D
 onready var aoe_hitbox = $Hitbox
 onready var aoe_hitbox_size = $Hitbox/CollisionShape2D
 onready var attack_particles = $Hitbox/CPUParticles2D
-onready var attack_timer = $AttackTimer
+onready var weapons = $Weapons
 
 #input
 var up
 var down
 var left
 var right
-var select
+var select 
 var cancel
 
-#how big the area around the player where enemies take damage is
+#how big the area around the player is for picking up items
 var aoe_size = 80
-
-#damage
-var max_damage = 10
-var attack_speed = 1
 
 #player movement
 var move_speed = 250
@@ -30,12 +26,11 @@ var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_update_weapon()
-	attack_timer.wait_time = attack_speed
+	_update_ring()
 
-# Called when active weapons need to be changed.
-func _update_weapon():
-	attack_particles.emission_sphere_radius = aoe_size
+# Called when pickup area size is changed.
+func _update_ring():
+	#attack_particles.emission_sphere_radius = aoe_size
 	aoe_hitbox_size.shape.radius = aoe_size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -48,6 +43,13 @@ func _process(delta):
 	select = Input.is_action_just_pressed("ui_accept")
 	cancel = Input.is_action_just_pressed("ui_cancel")
 	
+	if Input.is_action_just_pressed("ui_left"):
+		weapons.scale.x = -1
+		
+	if Input.is_action_just_pressed("ui_right"):
+		weapons.scale.x = 1
+	
+	print(scale.x)
 	#movement direction handler
 	if up or down or left or right:
 		if up:
@@ -71,21 +73,12 @@ func _process(delta):
 	velocity.x = lerp(velocity.x, direction.x, acceleration)
 	velocity.y = lerp(velocity.y, direction.y, acceleration)
 	
-	
 	move_and_slide(velocity * move_speed)
-
-
-# Called on attacks
-func attack():
 	
-	for body in aoe_hitbox.get_overlapping_bodies():
-		if body.is_in_group("Enemies"):
-			#TODO random damage from 0 to max damage
-			body.damage(max_damage)
-			print("attack!")
-	
-	pass
 
-# Auto-attack
-func _on_AttackTimer_timeout():
-	attack()
+
+func _on_Hitbox_area_entered(area):
+	if area.is_in_group("Pickups"):
+		if not area.has_player:
+			area.has_player = true
+			area.Player = self
