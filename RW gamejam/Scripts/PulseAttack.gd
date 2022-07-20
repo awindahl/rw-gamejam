fb830612c4f8cb527b88f79b703ed0cd7e19d505
Alexander Windahl <alewin1994@gmail.com>
2022-07-20 22:43:32 +0200

    reworked quake

--- RW gamejam/Scripts/PulseAttack.gd
+++ RW gamejam/Scripts/PulseAttack.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-onready var ground_particle_effect = preload("res://Scenes/ParticlePlayground.tscn")
+onready var ground_particle_effect = $QuakeParticle
 onready var attack_timer = $AttackTimer
-onready var particle_timer = $ParticleTimer
 onready var hitbox = $Area2D/CollisionShape2D
 onready var disable_timer = $AttackActive
 onready var start_timer = $AttackStart
@@ -13,7 +12,6 @@ var speed
 var projectiles
 var duration
 var attack_speed = 0.5
-var current_ground_particle = null
 var hitbox_size = 127
 var damage = 5
 
@@ -31,35 +29,32 @@ func _ready():
 
 # Called on attacks
 func attack():
+	ground_particle_effect.scale_amount = (hitbox_size/100 * 3.32)
+	ground_particle_effect.emitting = true
 	hitbox.disabled = false
 	disable_timer.start()
 
 
 func _on_AttackTimer_timeout():
 	attack_timer.start(attack_speed)
-	var new_ground_particle = ground_particle_effect.instance()
-	new_ground_particle.get_node("GroundPound").visible = true
-	new_ground_particle.scale *= 0.5
-	add_child(new_ground_particle)
-	current_ground_particle = new_ground_particle
-	particle_timer.start()
 	start_timer.start()
 
-
-func _on_ParticleTimer_timeout():
-	current_ground_particle.queue_free()
 
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Enemies"):
 		body.damage(damage)
 
+
 func _on_AttackActive_timeout():
 	hitbox.disabled = true
+	ground_particle_effect.one_shot = false
+	ground_particle_effect.emitting = false
 
 
 func _on_AttackStart_timeout():
 	attack()
+
 
 func level_up():
 	level += 1
@@ -71,3 +66,4 @@ func level_up():
 	projectiles = weaponData["projectiles"]
 	duration = weaponData["duration"]
 	attack_timer.wait_time = attack_speed
+	hitbox.shape.radius = hitbox_size
